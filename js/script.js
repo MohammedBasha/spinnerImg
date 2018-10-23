cb706b709ac4f810c996cd100d64d29e940bcfec
Mohammed Basha <mohammedbasha@bestresponsemedia.com>
2018-10-23 10:26:22 +0200

    set the load event handler for the high resolution image to remove the spinning image

--- js/script.js
+++ js/script.js
@@ -4,14 +4,16 @@ document.querySelector("img.preview").addEventListener("click", function(e) {
         highResImg = document.createElement("img"), // create an image for the high resolution version
         spinnerImg = document.createElement("img"); // create an image for the spinning one
 
-        overlayDiv.style.display = "block";
-        highResImg.className = "bgImg";
-        highResImg.src = lowResSrc.substr(0, lowResSrc.length - 7) + ".jpg";
-        overlayDiv.appendChild(highResImg);
+        overlayDiv.style.display = "block"; // show the overlay div
+        highResImg.className = "bgImg"; // add the predefined class to the high resolution image
+        highResImg.src = lowResSrc.substr(0, lowResSrc.length - 7) + ".jpg"; // set the src for the high resolution image
+        overlayDiv.appendChild(highResImg); // append the high resolution image to the overlay div
 
-        spinnerImg.className = "spinner";
-        spinnerImg.src = "images/spinner.gif";
-        overlayDiv.appendChild(spinnerImg);
+        spinnerImg.className = "spinner"; // add the predefined class to the spinning image
+        spinnerImg.src = "images/spinner.gif"; // set the src of the spinning image
+        overlayDiv.appendChild(spinnerImg); // append the spinning image in the overlay div
 
-        
+        highResImg.addEventListener("load", function(e) { // fire the load event handler on the high resolution image
+            spinnerImg.parentNode.removeChild(spinnerImg); // remove the spinning image since the high resolution image loads
+        }, false);
 }, false);
