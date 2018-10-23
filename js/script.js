0d3d3d50dbb526e2b31785c0443179409fdf7a38
Mohammed Basha <mohammedbasha@bestresponsemedia.com>
2018-10-23 10:35:43 +0200

    set a click event handler for the overlaydiv to disappear after clicking on it

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
-document.querySelector("img.preview").addEventListener("click", function(e) {
+// declare the overlay div globally to be used in the next both events
+var overlayDiv = document.querySelector("div.overlay"); // grab the overlay div
+
+document.querySelector("img.preview").addEventListener("click", function(e) { // start handling the click event on the low resolution image
+    
     var lowResSrc = e.target.src, // git the src of the targeted low resolution image
-        overlayDiv = document.querySelector("div.overlay"), // grab the overlay div
         highResImg = document.createElement("img"), // create an image for the high resolution version
         spinnerImg = document.createElement("img"); // create an image for the spinning one
 
-        overlayDiv.style.display = "block"; // show the overlay div
+        overlayDiv.style.display = "block"; // show the overlay div when click on the low resolution image
         highResImg.className = "bgImg"; // add the predefined class to the high resolution image
         highResImg.src = lowResSrc.substr(0, lowResSrc.length - 7) + ".jpg"; // set the src for the high resolution image
         overlayDiv.appendChild(highResImg); // append the high resolution image to the overlay div
@@ -16,4 +19,9 @@ document.querySelector("img.preview").addEventListener("click", function(e) {
         highResImg.addEventListener("load", function(e) { // fire the load event handler on the high resolution image
             spinnerImg.parentNode.removeChild(spinnerImg); // remove the spinning image since the high resolution image loads
         }, false);
+
+}, false);
+
+overlayDiv.addEventListener("click", function(){ // set the click event handler for the overlay div to disappear after clicking on it
+    this.style.display = "none";
 }, false);
